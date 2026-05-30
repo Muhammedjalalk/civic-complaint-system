72d6597b834827a8f31f9d6bd205ec3e9f1f902a
Muhammedjalalk <muhammedjalal928@gmail.com>
2026-05-30 22:15:32 +0530

    Change import of home view to accounts.views

--- backend/civic_system/urls.py
+++ backend/civic_system/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from .views import home  
+from accounts.views import home  
 
 urlpatterns = [
     path('', home),
